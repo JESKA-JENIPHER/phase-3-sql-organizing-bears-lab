ed5fd2ee7a2ff9beca74fc929d94b03cdd8500b0
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-08-22 14:58:44 +0300

    commit tree 06e0f2d715fe14b3d3081374b6a6f79df5b36586

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr.Chocolate", 5, "F", "black","calm",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy", 10, "F", "brown","angry",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha", 4, "M", "blue","wild",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown", 15, "F", "white","anxious",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melisa", 12, "M", "grey","calm",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch", 8, "M", "black","calm",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy", 9, "F", "black","angry",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL, 5, "F", "black","calm",1);
